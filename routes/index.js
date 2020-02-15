4f666d1c6f7ba6932ef88d34f30457239e48ddb8
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-15 15:45:52 +0530

    inserting the data

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,26 @@ router.get('/', function(req, res, next) {
   })
 });
 
+
+
+router.post('/', function(req, res, next) {
+  var empDetails=new empModel(
+    {
+      name:req.body.uname,
+      email:req.body.email,
+      etype:req.body.emptype,
+      hourlyrate:req.body.hrlyrate,
+      totalhous:req.body.ttlhr,
+    }
+  );
+ empDetails.save(function(err,res1)
+ {
+   if(err) throw err;
+   employee.exec(function(err,data)
+    {
+      if(err) throw err;
+    res.render('index', { title: 'Employee Records',records:data });
+  })
+ });//inserting the data
+});
 module.exports = router;
