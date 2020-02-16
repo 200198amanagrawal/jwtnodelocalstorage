3b8fbc261ea32462233aa369e343e4095908e0f6
Aman Agrawal <amanagrawal2098@gmail.com>
2020-02-16 11:12:22 +0530

    filtering the records

--- routes/index.js
+++ routes/index.js
@@ -33,4 +33,36 @@ router.post('/', function(req, res, next) {
   })
  });//inserting the data
 });
+router.post('/search/', function(req, res, next) {
+var filtername=req.body.fltrname;
+var filteremail=req.body.fltremail;
+var filteremptype=req.body.fltremptype;
+if(filtername!='' && filteremail!='' && filteremptype!='')//if nothing is empty
+{
+  var filterParameter={$and:[{name:filtername},
+  {$and:[{email:filteremail},{etype:filteremptype}]}
+]
+}
+}
+else if(filtername!='' && filteremptype!='')//only email can be null
+{
+  var filterParameter={$and:[{name:filtername},{etype:filteremptype}]
+}
+}
+else if(filteremail!='' && filteremptype!='')//only name can be null
+{
+  var filterParameter={$and:[{name:filteremail},{etype:filteremptype}]
+}
+}
+else
+{
+  var filterParameter={};
+}
+var employeeFilter=empModel.find(filterParameter);
+employeeFilter.exec(function(err,data)
+{
+      if(err) throw err;
+    res.render('index', { title: 'Employee Records',records:data });
+  });
+});
 module.exports = router;
